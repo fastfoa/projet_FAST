98733fff6ba883e781f2fe20ecb4b02704059688
xavier <xavier.bourget@foreach-academy.fr>
2022-01-31 15:35:57 +0100

    avant maquette

---
+++ src/Controller/LanController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LanController extends AbstractController
+{
+    /**
+     * @Route("/lan", name="lan")
+     */
+    public function index(): Response
+    {
+        return $this->render('lan/base.html.twig', [
+            'controller_name' => 'LanController',
+        ]);
+    }
+
+
+public function login(): Response
+{
+    return $this->render('lan/login.html.twig', [
+        'controller_name' => 'LanController',
+    ]);
+}
+
+public function registerStudent(): Response
+{
+    return $this->render('lan/registerStudent.html.twig', [
+        'controller_name' => 'LanController',
+    ]);
+}
+
+public function dashBoardStudent(): Response
+{
+    return $this->render('lan/dashBoardStudent.html.twig', [
+        'controller_name' => 'LanController',
+    ]);
+}
+
+public function dashBoardSchool(): Response
+{
+    return $this->render('lan/dashBoardSchool.html.twig', [
+        'controller_name' => 'LanController',
+    ]);
+}
+
+
+
+}
