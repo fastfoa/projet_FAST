1c4c7976635822e7ba51902bac29971636a0fb35
karimm <karimm991@gmail.com>
2022-02-09 20:37:55 +0100

    recupération de l'entité Evaluation

---
+++ src/Entity/Evaluation.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EvaluationRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=EvaluationRepository::class)
+ */
+class Evaluation
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $remarqueMS;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $remarqueApp;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $remarqueOF;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $note;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getRemarqueMS(): ?string
+    {
+        return $this->remarqueMS;
+    }
+
+    public function setRemarqueMS(string $remarqueMS): self
+    {
+        $this->remarqueMS = $remarqueMS;
+
+        return $this;
+    }
+
+    public function getRemarqueApp(): ?string
+    {
+        return $this->remarqueApp;
+    }
+
+    public function setRemarqueApp(string $remarqueApp): self
+    {
+        $this->remarqueApp = $remarqueApp;
+
+        return $this;
+    }
+
+    public function getRemarqueOF(): ?string
+    {
+        return $this->remarqueOF;
+    }
+
+    public function setRemarqueOF(?string $remarqueOF): self
+    {
+        $this->remarqueOF = $remarqueOF;
+
+        return $this;
+    }
+
+    public function getNote(): ?int
+    {
+        return $this->note;
+    }
+
+    public function setNote(?int $note): self
+    {
+        $this->note = $note;
+
+        return $this;
+    }
+}
