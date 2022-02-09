cc43edc50fef8c5e9ae0ad0e3359c8ce2bf00588
Mehdi2707 <mehdi2707@live.fr>
2022-02-09 16:18:39 +0100

    maj

---
+++ src/Entity/Document.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\DocumentRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=DocumentRepository::class)
+ */
+class Document
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $ficName;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $titre;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $dateCreate;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFicName(): ?string
+    {
+        return $this->ficName;
+    }
+
+    public function setFicName(string $ficName): self
+    {
+        $this->ficName = $ficName;
+
+        return $this;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): self
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    public function getDateCreate(): ?\DateTimeInterface
+    {
+        return $this->dateCreate;
+    }
+
+    public function setDateCreate(\DateTimeInterface $dateCreate): self
+    {
+        $this->dateCreate = $dateCreate;
+
+        return $this;
+    }
+}
