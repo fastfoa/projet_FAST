c3c57613ecd7245c3f776d6e19cc23f3fad47c55
karimm <karimm991@gmail.com>
2022-02-11 17:08:53 +0100

    sauvegarde travail du jour: table competences et fixtures plus lien

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,9 +2,11 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Competences;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use App\Entity\Formation;
+
 
 class AppFixtures extends Fixture
 {
@@ -22,6 +24,77 @@ class AppFixtures extends Fixture
         $f->setNom("Bachelor");
         $manager->persist($f);
 
+
+
+        $competences = new Competences();
+        $competences->setCompetence("Capacité d'adaptation.");
+        $competences->setIdFormation(1);
+        $manager->persist($competences);
+
+        $competences = new Competences();
+        $competences->setCompetence("Gestion du stress.");
+        $competences->setIdFormation(1);
+        $manager->persist($competences);
+
+        $competences = new Competences();
+        $competences->setCompetence("Travail en équipe.");
+        $competences->setIdFormation(1);
+        $manager->persist($competences);
+
+        $competences = new Competences();
+        $competences->setCompetence("Sens de l'organisation.");
+        $competences->setIdFormation(1);
+        $manager->persist($competences);
+
+        $competences = new Competences();
+        $competences->setCompetence("Autonomie.");
+        $competences->setIdFormation(1);
+        $manager->persist($competences);
+
+       
+        $competences = new Competences();
+        $competences->setCompetence("Maquetter une application.");
+        $competences->setIdFormation(1);
+        $manager->persist($competences);
+
+        $competences = new Competences();
+        $competences->setCompetence("Réaliser une interface utilisateur web statique et adaptable.");
+        $competences->setIdFormation(1);
+        $manager->persist($competences);
+
+        $competences = new Competences();
+        $competences->setCompetence("Développer une interface utilisateur web dynamique.");
+        $competences->setIdFormation(1);
+        $manager->persist($competences);
+
+        $competences = new Competences();
+        $competences->setCompetence("Réaliser une interface utilisateur avec une solution de gestion de contenu ou e-commerce.");
+        $competences->setIdFormation(1);
+        $manager->persist($competences);
+
+        $competences = new Competences();
+        $competences->setCompetence("Créer une base de données.");
+        $competences->setIdFormation(1);
+        $manager->persist($competences);
+
+        $competences = new Competences();
+        $competences->setCompetence("Développer les composants d'accès aux données.");
+        $competences->setIdFormation(1);
+        $manager->persist($competences);
+
+        $competences = new Competences();
+        $competences->setCompetence("Développer la partie back-end d'une application web ou web mobile.");
+        $competences->setIdFormation(1);
+        $manager->persist($competences);
+
+        $competences = new Competences();
+        $competences->setCompetence("Elaborer et mettre en œuvre des composants dans une application de gestion de contenu ou e-commerce.");
+        $competences->setIdFormation(1);
+        $manager->persist($competences);
+
         $manager->flush();
     }
+
+
+
 }
---
+++ src/Entity/Competences.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CompetencesRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=CompetencesRepository::class)
+ */
+class Competences
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $idFormation;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $competence;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getIdFormation(): ?int
+    {
+        return $this->idFormation;
+    }
+
+    public function setIdFormation(int $idFormation): self
+    {
+        $this->idFormation = $idFormation;
+
+        return $this;
+    }
+
+    public function getCompetence(): ?string
+    {
+        return $this->competence;
+    }
+
+    public function setCompetence(string $competence): self
+    {
+        $this->competence = $competence;
+
+        return $this;
+    }
+}
