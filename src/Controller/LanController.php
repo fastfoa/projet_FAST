1de04e2649537a407ce787caaeec5d958a7d9db4
xavier <xavier.bourget@foreach-academy.fr>
2022-02-22 09:50:37 +0100

    annuaire add SQLlibrary src/lib/PDO 2

--- src/Controller/LanController.php
+++ src/Controller/LanController.php
@@ -12,7 +12,7 @@ use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use App\Repository\UserRepository;
 use ContainerT2hE2KD\getDoctrine_QueryDqlCommandService;
-use PDO;
+use App\Lib\PDO;
 
 use App\Form\FormateurType;
 
@@ -149,119 +149,10 @@ class LanController extends AbstractController
         return $this->render('lan/annuaire.html.twig');
     }
 
-    public function annuaireDataPDO(): Response
+    public function annuaireData(): Response
     {
-        //https://www.doctrine-project.org/projects/doctrine-orm/en/2.9/reference/native-sql.html
-        $dsn = "mysql:host=127.0.0.1;dbname=projet_FAST";
-        try {
-             $pdo = new PDO($dsn, 'xxx', 'xxx');
-        } catch (\PDOException $e) 
-        {
-             throw new \PDOException($e->getMessage(), (int)$e->getCode());
-        }
-        $rs = $pdo->prepare('SELECT nom FROM  user where roles=JSON({"ROLE_APP"}) '   );
-        $rs = $pdo->prepare('SELECT nom FROM  user' );
-        $rs->execute();
-        $user=[];
-        while(   $ligne = $rs->fetch(PDO::FETCH_ASSOC) )
-        {
-            if ( $ligne[  'nom' ] != null)
-                $user[] = $ligne[  'nom' ];
-        }
-
-        //return  new Response(  json_encode( $user )   );
+        $user = getSQLArrayAssoc( 'SELECT nom FROM  user' );    
         return  new JsonResponse(   $user    );
-    }
-
-
-    
-    //annuaireDatar()
-    public function annuaireDataRepoNativSQL( UserRepository $userRepository )
-    {
-        /*
-        //$em  = $em->getEntityManager();
-        $rsm = new ResultSetMapping();
-        $query = $em->createNativeQuery('SELECT u.nom FROM user u where nom <> ?', $rsm);
-        $query->setParameter(1,  'jm' );
-                
-        $users =  $query->getResult();;
-        return  new JsonResponse(   $users    );
-        */
-    }
-
-    public function annuaireDataRepoTest( UserRepository $userRepository ): Response
-    {
-        //ManagerRegistry $doctrine
-        //$doctrine = $this->getDoctrine();
-
-        //$products = $registry->getRepository(Product::class)->annuaireDatar();
-        //$entityManager = $registry->getManager();
-
-        //$sm =new ServiceEntityRepository( $registry, User::class );
-        //$entityManager = $sm->getEntityManager();
-
-        //$this->getDoctrine()->getRepository();
-
-        $user = $userRepository->annuaireData( 'MODE_APP' );
-        return  new JsonResponse(   $user    );
-
-        /*
-        $conn = $this->getEntityManager()->getConnection();
-
-        $sql = '
-            SELECT * FROM product p
-            WHERE p.price > :price
-            ORDER BY p.price ASC
-            ';
-        $stmt = $conn->prepare($sql);
-        $resultSet = $stmt->executeQuery(['price' => $price]);
-        */
-        // returns an array of arrays (i.e. a raw data set)
-        //return $resultSet->fetchAllAssociative()
-
-        // returns an array of Product objects
-        //return $query->getResult();
-    }
-
-    public function annuaireDataConnection( Connection $connection ): Response
-    {
-        $users = $connection->fetchAll('SELECT * FROM users');
-
-        // ...
-    }
-
-
-
-    //public function annuaireDataRepoRegistry( ManagerRegistry $registry ): Response
-    public function annuaireData( ManagerRegistry $registry ): Response
-    {
-        //$userRepository = $registry->getRepository(User::class);
-        //$user = $userRepository->annuaireData();
-        //$em = $registry->getManager();
-
-        //$sm =new ServiceEntityRepository( $registry, User::class );
-        //$em = $sm->getEntityManager();
-
-        //$this->getDoctrine()->getRepository();
-
-        //$user = $userRepository->annuaireData( 'MODE_APP' );
-        //return  new JsonResponse(   $user    );
-
-        $conn = $em->getConnection();
-
-        $sql = '
-            SELECT nom FROM user u
-            WHERE p.price > :price
-            ORDER BY p.price ASC
-            ';
-        $sql = '
-            SELECT nom FROM user u';
-
-        $stmt = $conn->prepare($sql);
-
-        //$resultSet = $stmt->executeQuery(['price' => $price]);
-        $resultSet = $stmt->executeQuery();
-        return  new JsonResponse(   $resultSet    );
     }
 
     //annuaireDatar()
@@ -271,10 +162,4 @@ class LanController extends AbstractController
         $user = $userRepository->annuaireDataNative( 'MODE_APP' );
         return  new JsonResponse(   $user    );
     }
-
-
-
-
-
-
 }
