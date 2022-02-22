276c2c6e2fcc667090fd0d12f57d6f59b352f0a1
karim <karim-megri@hotmail.fr>
2022-02-22 14:15:59 +0100

    EvaluationController et saisiEvaluation ok

--- src/Controller/EvaluationController.php
+++ src/Controller/EvaluationController.php
@@ -10,9 +10,15 @@ use App\Entity\Competence;
 use App\Entity\User;
 use App\Entity\Session;
 use App\Form\EvaluationType;
+use Symfony\Component\HttpFoundation\Request;
+
+
+
+
 
 class EvaluationController extends AbstractController
 {
+    
     /**
      * @Route("/evaluation", name="evaluation")
      */
@@ -23,22 +29,37 @@ class EvaluationController extends AbstractController
         ]);
     }
 
-    public function saisiEvaluation(Competence $competence, User $app, Session $session ): Response
-    {   
+    public function saisiEvaluation(Competence $competence, User $app, Session $session, Request $request): Response
+    {
+        $message = false;
         $evaluation = new Evaluation();
-
-        $evaluation->setIdCompetence( $competence->getId() );
-        $evaluation->setIdApp( $app->getId() );
-        $evaluation->setIdSession( $session->getId() );
-
+       
+        $nameCompet = $competence->getName();
+        $evaluation->setIdCompetence($competence->getId());
+        $evaluation->setIdApp($app->getId());
+        $evaluation->setIdMA(1);
+        $evaluation->setIdFormateur(1);
+        $evaluation->setIdSession($session->getId(1));
 
         $form = $this->createForm(EvaluationType::class, $evaluation);
-        $nameCompet = $competence->getName();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            
+            $doctrine = $this->getDoctrine();
+            $entityManager = $doctrine->getManager();
+            $entityManager->persist($evaluation);
+            $entityManager->flush();
+            $message = "le formulaire a bien était pris en compte ";
+            
+        }
 
         return $this->render('evaluation/saisiEvaluation.html.twig', [
-            'form' => $form->createView(),
-            'nameCompet'=>$nameCompet
-
-        ]);
+                'form' => $form->createView(),
+                'nameCompet' => $nameCompet,
+                'message' => $message
+            ]);
+        
     }
 }
