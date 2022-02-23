aff07c1ebdec04e79440edfed4e3e6fa2826c288
xavier <xavier.bourget@foreach-academy.fr>
2022-02-23 11:38:41 +0100

    add faker

--- src/Controller/ModifInfoPersoController.php
+++ src/Controller/ModifInfoPersoController.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Controller;
 
 use App\Entity\User;
@@ -14,6 +13,8 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
+use App\Lib\Form;
+
 class ModifInfoPersoController extends AbstractController
 {
     private $entityManager;
@@ -21,243 +22,75 @@ class ModifInfoPersoController extends AbstractController
     {
         $this->entityManager = $entityManager;
     }
-    
-    public function monCompteAPP(Request $request, UserPasswordEncoderInterface $encoder): Response
+
+    public function monCompte(Request $request, UserPasswordEncoderInterface $encoder): Response
     {
         $notification = null;
         $contact = $this->getUser();
-        $form = $this->createForm(MonCompteAppType::class, $contact);
+
+        $role = $contact->getRoles()[0];
+
+        //dd( $role );
+        $type =  null;
+        $twig = ''; 
+
+
+        if ( $role == 'ROLE_APP')
+        {
+            $type =  MonCompteAppType::class;
+            $twig = 'mon_compte/monCompteApp.html.twig'; 
+            $titre = "Apprenti";
+        }
+        else if ( $role == 'ROLE_MA')
+        {
+            $type =  MonCompteMAType::class;
+            $twig = 'mon_compte/monCompteMA.html.twig'; 
+            $titre = "Maitre d'Apprentissage";
+        }
+        else if ( $role == 'ROLE_ENT')
+        {
+            $type =  MonCompteEntrepriseType::class;
+            $twig = 'mon_compte/monCompteEntreprise.html.twig';
+            $titre = "Entreprise";
+ 
+        }
+        else if ( $role == 'ROLE_FORMATEUR')
+        {
+            $type =  MonCompteFormateurType::class;
+            $twig = 'mon_compte/monCompteFormateur.html.twig';
+            $titre = "Formateur";
+        }
+        
+        
+
+        $form = $this->createForm($type, $contact);
+
+
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
-            
-            
-                //enregistrer la nouvelle email
-                $new_email = $form->get('email')->getData();
-                $new_email = strip_tags($new_email);
-                $contact->setEmail($new_email);
 
-                //enregistrer la nouvelle adresse
-                $new_adresse = $form->get('adresse')->getData();
-                $new_adresse = strip_tags($new_adresse);
-                $contact->setAdresse($new_adresse);
+            $contact = filtreForm($form, $contact);
 
-                
-
-                //enregistrer le nouveaux telephone
-                $new_telephone = $form->get('telephone')->getData();
-                $new_telephone = strip_tags($new_telephone);
-                $contact->setTelephone($new_telephone);
-
-
-                //enregistrer le nouveaux new_RepresentantLegal1
-                $new_RepresentantLegal1 = $form->get('RepresentantLegal1')->getData();
-                $new_RepresentantLegal1 = strip_tags($new_RepresentantLegal1);
-                $contact->setRepresentantLegal1($new_RepresentantLegal1);
-
-                //enregistrer le nouveaux new_RepresentantLegal2
-                $new_RepresentantLegal2 = $form->get('RepresentantLegal2')->getData();
-                $new_RepresentantLegal2 = strip_tags($new_RepresentantLegal2);
-                $contact->setRepresentantLegal2($new_RepresentantLegal2);
-
-                //enregistrer le nouveaux tavailleurhadicapé
-                $new_TravailleurHandicape = $form->get('TravailleurHandicape')->getData();
-                $new_TravailleurHandicape = strip_tags($new_TravailleurHandicape);
-                $contact->setTravailleurHandicape($new_TravailleurHandicape);
-
-                //enregistrer le nouveaux SportifHautNiveau
-                $new_SportifHautNiveau = $form->get('SportifHautNiveau')->getData();
-                $new_SportifHautNiveau = strip_tags($new_SportifHautNiveau);
-                $contact->setSportifHautNiveau($new_SportifHautNiveau);
-
-
-                //mdp
-                $old_pwd = $form->get('old_password')->getData();
-
+            $old_pwd = $form->get('old_password')->getData();
             if ($encoder->isPasswordValid($contact, $old_pwd)) {
 
                 // modification du mot de pass 
 
-                $new_pwd= $form->get('new_password')->getData();
+                $new_pwd = $form->get('new_password')->getData();
                 $password = $encoder->encodePassword($contact, $new_pwd);
                 $contact->setPassword($password);
-
-                $this->entityManager->persist($contact);
-                // $doctrine = $this->getDoctrine();
-                // $entityManager = $doctrine->getManager();
-                $this->entityManager->flush();
-                $notification = "Vos information ont  été mis à jour.";
             }
+
+            $this->entityManager->persist($contact);
+            $this->entityManager->flush();
+            $notification = "Vos information ont  été mis à jour.";
         }
-        return $this->render('mon_compte/monCompteApp.html.twig',[
+        return $this->render($twig, [
             'form' => $form->createView(),
-            'notification' => $notification
+            'notification' => $notification,
+            //'titre' =>   $titre
+
         ]);
     }
-    public function monCompteEntreprise(Request $request, UserPasswordEncoderInterface $encoder): Response
-    {
-        $notification = null; 
-        $contact = $this->getUser();
-        $form = $this->createForm(MonCompteEntrepriseType::class, $contact);
-        $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) 
-        {
-                //enregistrer la nouvelle email
-                $new_email = $form->get('email')->getData();
-                $new_email = strip_tags($new_email);
-                $contact->setEmail($new_email);
 
-                //enregistrer le nom nouveaux nom
-                $new_nom = $form->get('nom')->getData();
-                $new_nom = strip_tags($new_nom);
-                $contact->setNom($new_nom);
-
-                //enregistrer la nouvelle adresse
-                $new_adresse = $form->get('adresse')->getData();
-                $new_adresse = strip_tags($new_adresse);
-                $contact->setAdresse($new_adresse);
-
-                //enregistrer le nouveaux telephone
-                $new_telephone = $form->get('telephone')->getData();
-                $new_telephone = strip_tags($new_telephone);
-                $contact->setTelephone($new_telephone);
-
-               
-
-                //enregistrer le nouveaux Effectif
-                $new_Effectif = $form->get('Effectif')->getData();
-                $new_Effectif = strip_tags($new_Effectif);
-                $contact->setEffectif($new_Effectif);
-
-                //enregistrer le nouveaux Public|| Public
-                $new_EmployeurPublic = $form->get('EmployeurPublic')->getData();
-                $new_EmployeurPublic = strip_tags($new_EmployeurPublic);
-                $contact->setEmployeurPublic($new_EmployeurPublic);
-
-                // modification du mot de pass 
-                $old_pwd = $form->get('old_password')->getData();
-
-            if ($encoder->isPasswordValid($contact, $old_pwd)) {
-                // modification du mot de pass 
-
-                $new_pwd= $form->get('new_password')->getData();
-                $password = $encoder->encodePassword($contact, $new_pwd);
-                $contact->setPassword($password);
-
-                $this->entityManager->persist($contact);
-                // $doctrine = $this->getDoctrine();
-                // $entityManager = $doctrine->getManager();
-                $this->entityManager->flush();
-                $notification = "Vos information ont  été mis à jour.";
-            }
-        }
-        return $this->render('mon_compte/monCompteEntreprise.html.twig', [
-            'form' => $form->createView(),
-        ]);
-    }
-    public function monCompteMA(Request $request, UserPasswordEncoderInterface $encoder): Response
-    {
-        $notification = null; 
-        $contact = $this->getUser();
-        $form = $this->createForm(MonCompteMAType::class, $contact);
-        $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) 
-        {
-
-            //enregistrer la nouvelle adresse
-            $new_adresse = $form->get('adresse')->getData();
-            $new_adresse = strip_tags($new_adresse);
-            $contact->setAdresse($new_adresse);
-            
-            //enregistrer le nom nouveaux prenom
-            $new_prenom = $form->get('prenom')->getData();
-            $new_prenom = strip_tags($new_prenom);
-            $contact->setPrenom($new_prenom);
-
-            //enregistrer le nouveaux telephone
-            $new_telephone = $form->get('telephone')->getData();
-            $new_telephone = strip_tags($new_telephone);
-            $contact->setTelephone($new_telephone);
-
-            //enregistrer le nom nouveaux nom
-            $new_nom = $form->get('nom')->getData();
-            $new_nom = strip_tags($new_nom);
-            $contact->setNom($new_nom);
-
-            //enregistrer la nouvelle email
-            $new_email = $form->get('email')->getData();
-            $new_email = strip_tags($new_email);
-            $contact->setEmail($new_email);
-
-             // modification du mot de pass 
-             $old_pwd = $form->get('old_password')->getData();
-
-             if ($encoder->isPasswordValid($contact, $old_pwd)) {
-                 // modification du mot de pass 
- 
-                 $new_pwd= $form->get('new_password')->getData();
-                 $password = $encoder->encodePassword($contact, $new_pwd);
-                 $contact->setPassword($password);
- 
-                 $this->entityManager->persist($contact);
-                 // $doctrine = $this->getDoctrine();
-                 // $entityManager = $doctrine->getManager();
-                 $this->entityManager->flush();
-                 $notification = "Vos information ont  été mis à jour.";
-             }
-        }
-        return $this->render('mon_compte/monCompteMA.html.twig',[
-            'form' => $form->createView()
-        ]);
-    }
-    public function monCompteFormateur(Request $request, UserPasswordEncoderInterface $encoder): Response
-    {
-        $contact = $this->getUser();
-        $form = $this->createForm(MonCompteFormateurType::class, $contact);
-        $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) 
-        {
-
-
-            //enregistrer la nouvelle adresse
-            $new_adresse = $form->get('adresse')->getData();
-            $new_adresse = strip_tags($new_adresse);
-            $contact->setAdresse($new_adresse);
-
-            //enregistrer le nouveaux telephone
-            $new_telephone = $form->get('telephone')->getData();
-            $new_telephone = strip_tags($new_telephone);
-            $contact->setTelephone($new_telephone);
-
-            //enregistrer le nom nouveaux nom
-            $new_nom = $form->get('nom')->getData();
-            $new_nom = strip_tags($new_nom);
-            $contact->setNom($new_nom);
-
-
-            //enregistrer la nouvelle email
-            $new_email = $form->get('email')->getData();
-            $new_email = strip_tags($new_email);
-            $contact->setEmail($new_email);
-
-             // modification du mot de pass 
-             $old_pwd = $form->get('old_password')->getData();
-
-             if ($encoder->isPasswordValid($contact, $old_pwd)) {
-                 // modification du mot de pass 
- 
-                 $new_pwd= $form->get('new_password')->getData();
-                 $password = $encoder->encodePassword($contact, $new_pwd);
-                 $contact->setPassword($password);
- 
-                 $this->entityManager->persist($contact);
-                 // $doctrine = $this->getDoctrine();
-                 // $entityManager = $doctrine->getManager();
-                 $this->entityManager->flush();
-                 $notification = "Vos information ont  été mis à jour.";
-             }
-        }
-        return $this->render('mon_compte/monCompteFormateur.html.twig',[
-            'form' => $form->createView()
-        ]);
-    }
 }
