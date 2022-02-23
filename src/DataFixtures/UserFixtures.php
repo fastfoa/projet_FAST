2460c4bdcb1f98772d61e06326a2bde19584e3b9
karim <karim-megri@hotmail.fr>
2022-02-23 11:01:26 +0100

    ok ok ok

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -57,10 +57,12 @@ class UserFixtures extends Fixture
         $user->setEffectif(500);
         $user->setConventionCollective("Convention collective");
         $user->setEmployeurPublic(false);
+        
         $user->setCodeIDCCConvention("8905");
         $manager->persist($user);
 
         // Fixture Maître d'apprentissage
+        for ($i = 0; $i <= 10; $i++) {
         $user = new User();
         $user->setEmail("the@maitre.fr");
         $user->setRoles(["ROLE_MA"]);
@@ -70,9 +72,17 @@ class UserFixtures extends Fixture
         $user->setAdresse("197 rue symfony");
         $user->setTelephone("0604274604");
         $user->setDateNaissance(new \DateTime());
+        $user->setLinkedin("http://www.linkedin.com");
+        $user->setGithub("http://www.github.com");
+        $user->setSiteWebPro("http://www.mywebsite.com");
+        $user->setFonctionMA("dev");
+        $user->setRGPDOK(true);
+        $user->setProfilEnabled(true);
         $manager->persist($user);
+        }
 
         // Fixture Formateur
+        for ($i = 0; $i <= 3; $i++) {
         $user = new User();
         $user->setEmail("xavier@gmail.fr");
         $user->setRoles(["ROLE_FORMATEUR"]);
@@ -84,27 +94,72 @@ class UserFixtures extends Fixture
         $user->setDiplome("bac+10");
         $user->setDateNaissance(new \DateTime());
         $user->setGenre("M");
+        $user->setLinkedin("http://www.linkedin.com");
+        $user->setGithub("http://www.github.com");
+        $user->setSiteWebPro("http://www.mywebsite.com");
+        $user->setRGPDOK(true);
+        $user->setProfilEnabled(true);
         $manager->persist($user);
+        }
 
         // Fixture Organisme de formation
+        
         $user = new User();
-        $user->setEmail("foreach@academy.fr");
+        $user->setEmail("organisme@formation.fr");
         $user->setRoles(["ROLE_OF"]);
         $user->setPassword('$2y$13$psh41BRs9DM2NbQbmswERufwjiWzX8WNqjGvIFgqGimHTNunlGSHm'); // 789
-        $user->setNom("Foreach Academy");
-        $user->setAdresse("393 Rue du général de Gaulle");
-        $user->setTelephone("0342215457");
+        $user->setSiret("rr4ndgrg45");
         $user->setCFA(true);
-        $user->setDenominationCFAResponsable("foreachacademy - symbolit");
-        $user->setNumeroUAICFA("dgjd66sd");
+        $user->setAdresse("39 Rue du général");
+        $user->setTelephone("0342289457");
+        $user->setDenominationCFAResponsable("CFA Nord");
+        $user->setNumeroUAICFA("dgjdg8sd");
+        $user->setDiplomeVise("developpeur web et web mobile");
+        $user->setIntitulePreciDiplomevise("Titre Professionnel de développeur web et web mobile");
+        $user->setLinkedin("http://www.linkedin.com");
+        $user->setGithub("http://www.github.com");
+        $user->setSiteWebPro("http://www.mywebsite.com");
+        $user->setRGPDOK(true);
+        $user->setProfilEnabled(true);
+        $manager->persist($user);
         $manager->persist($user);
 
         // Fixture Admin
+        for ($i = 0; $i <= 3; $i++) {
         $user = new User();
         $user->setEmail("super@admin.fr");
         $user->setRoles(["ROLE_ADMIN"]);
         $user->setPassword('$2y$13$psh41BRs9DM2NbQbmswERufwjiWzX8WNqjGvIFgqGimHTNunlGSHm'); // 789
         $manager->persist($user);
+        }
+
+         // Fixture Indépendant
+
+         for ($i = 0; $i <= 10; $i++) {
+         $user = new User();
+         $user->setEmail("you@entreprise.fr");
+         $user->setRoles(["ROLE_IND"]);
+         $user->setPassword('$2y$13$Td/hy38QDVNeXhaQIz.jZOU23o0HVOe8LDLuYAK9aXQDYeNLff5aO'); // 0123
+         $user->setNom("Zuckerberg");
+         $user->setRaisonSocial("Facebook");
+         $user->setPrenom("Marc");
+         $user->setFonctionMA("PDG");
+         $user->setDateNaissance(new \DateTime());
+         $user->setAdresse("45 rue du typescript");
+         $user->setTelephone("0728756614");
+         $user->setSiret("54u5th5t4jrjrjkt5");
+         $user->setNAF("dj580");
+         $user->setEffectif(500);
+         $user->setConventionCollective("Convention collective");
+         $user->setEmployeurPublic(false);
+         $user->setCodeIDCCConvention("8905");
+         $user->setLinkedin("http://www.linkedin.com");
+         $user->setGithub("http://www.github.com");
+         $user->setSiteWebPro("http://www.mywebsite.com");
+         $user->setRGPDOK(true);
+         $user->setProfilEnabled(true);
+         $manager->persist($user); 
+        }
 
         // Fixture Apprenant
         for ($i = 0; $i <= 20; $i++) {
@@ -132,6 +187,13 @@ class UserFixtures extends Fixture
             $user->setDiplomePlusHaut("bac+5");
             $user->setDiplomeVise("Concepteur developpeur d'applications");
             $user->setIntitulePreciDiplomevise("Titre professionnel de concepteur et développeur d'applications");
+            $user->setRepresentantLegal1("Toto");
+            $user->setRepresentantLegal2("Tata");
+            $user->setLinkedin("http://www.linkedin.com/%22");
+            $user->setGithub("http://www.github.com/%22");
+            $user->setSiteWebPro("http://www.mywebsite.com/%22");
+            $user->setRGPDOK($faker->boolean);
+            $user->setProfilEnabled(true);
             $manager->persist($user);
         }
 
@@ -143,7 +205,7 @@ class UserFixtures extends Fixture
             $user = new User();
             $user->setEmail($faker->companyEmail);
             $user->setRoles(["ROLE_ENT"]);
-            $user->setPassword( '000' ); // 0123
+            $user->setPassword('000'); // 0123
             $user->setNom($faker->company);
             $user->setAdresse($faker->address);
             $user->setTelephone($faker->phoneNumber);
@@ -153,6 +215,11 @@ class UserFixtures extends Fixture
             $user->setConventionCollective("Convention collective 66");
             $user->setEmployeurPublic($faker->boolean);
             $user->setCodeIDCCConvention("8905");
+            $user->setLinkedin("http://www.linkedin.com");
+            $user->setGithub("http://www.github.com");
+            $user->setSiteWebPro("http://www.mywebsite.com");
+            $user->setRGPDOK(true);
+            $user->setProfilEnabled(true);
             $manager->persist($user);
         }
         $manager->flush();
