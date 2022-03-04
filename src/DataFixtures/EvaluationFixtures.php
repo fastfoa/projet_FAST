356fc2f1849662523750543cb52e7b52db7b68b2
xavier <xavier.bourget@foreach-academy.fr>
2022-03-04 12:14:57 +0100

    merge avec symfo

--- src/DataFixtures/EvaluationFixtures.php
+++ src/DataFixtures/EvaluationFixtures.php
@@ -35,6 +35,40 @@ class EvaluationFixtures extends Fixture
         }
 
 
+        for ($i = 0; $i < 5; $i++) {
+            $evaluation = new Evaluation();
+            $evaluation->setIdCompetence($faker->numberBetween(1, 13));
+            $evaluation->setIdApp($faker->numberBetween(1, 20));
+            $evaluation->setIdMA($faker->numberBetween(101, 121));
+            $evaluation->setIdFormateur($faker->numberBetween(201, 221));
+            $evaluation->setIdSession($faker->numberBetween(1, 3));
+            $evaluation->setDateApp($faker->dateTimeBetween('-1 years', 'now', $timezone = null));
+            $evaluation->setDateFormateur($faker->dateTimeBetween('-1 years', 'now', $timezone = null));
+            $evaluation->setDateOF($faker->dateTimeBetween('-1 years', 'now', $timezone = null));
+            $evaluation->setNote($faker->numberBetween(5, 20));
+            $evaluation->setRemarqueAPP($faker->text);
+            $evaluation->setRemarqueFormateur($faker->text);
+            $evaluation->setRemarqueOF($faker->text);
+            $manager->persist($evaluation);
+        }
+
+        for ($i = 0; $i < 5; $i++) {
+            $evaluation = new Evaluation();
+            $evaluation->setIdCompetence($faker->numberBetween(1, 13));
+            $evaluation->setIdApp($faker->numberBetween(1, 20));
+            $evaluation->setIdMA($faker->numberBetween(101, 121));
+            $evaluation->setIdFormateur($faker->numberBetween(201, 221));
+            $evaluation->setIdSession($faker->numberBetween(1, 3));
+            $evaluation->setDateApp($faker->dateTimeBetween('-1 years', 'now', $timezone = null));
+            $evaluation->setDateMA($faker->dateTimeBetween('-1 years', 'now', $timezone = null));
+            $evaluation->setNote($faker->numberBetween(5, 20));
+            $evaluation->setRemarqueAPP($faker->text);
+            $evaluation->setRemarqueMA($faker->text);
+            $manager->persist($evaluation);
+        }
+
+
+
 
         $manager->flush();
     }
